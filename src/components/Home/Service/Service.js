ae731486cbc4e1a8d686da5fa0ce9de53c5f6e44
sharifulislam565619 <sharifulislam565619@gmail.com>
2021-10-19 18:28:54 +0600

    service cart add animation

--- src/components/Home/Service/Service.js
+++ src/components/Home/Service/Service.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import { Card, CardGroup, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import './Service.css';
 
 const Service = (props) => {
     const { id, service, img, fee, desc } = props.service
     return (
         <Col xl={3} lg={3} md={6} sm={12}>
             <CardGroup>
-                <Card className="py-2">
+                <Card className="py-2 data-cart">
 
                     <Card.Img variant="top" className="w-75 mx-auto" src={img} />
 
                     <Card.Body>
                         <Card.Title><h6><strong>{service}</strong></h6></Card.Title>
-                        <Card.Title><h5>{fee}</h5></Card.Title>
+                        <Card.Title><h6>Fee: {fee}</h6></Card.Title>
                         <Card.Text>
                             <small> {desc.slice(0, 55)}...</small>
                         </Card.Text>
